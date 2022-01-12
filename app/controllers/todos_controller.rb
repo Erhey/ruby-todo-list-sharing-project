39513f3e278e36b63869acea19ee970d97df7780
Erhey <kevin.martin.eng@outlook.com>
2022-01-12 20:24:04 +0900

    CRUD DONE

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,56 @@
+class TodosController < ApplicationController
+
+    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+    def index
+      @articles = Article.all
+    end
+
+
+    def index
+      @todos = Todo.all
+    end
+    def show
+      @todo = Todo.find(params[:id])
+    end
+  
+    def new
+      @todo = Todo.new
+    end
+    def create
+      @todo = Todo.new(todo_params)
+      if @todo.save
+        redirect_to @todo
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+  
+    def edit
+      @todo = Todo.find(params[:id])
+    end
+  
+    def update
+      @todo = Todo.find(params[:id])
+  
+      if @todo.update(todo_params)
+        redirect_to @todo
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @todo = Todo.find(params[:id])
+      @todo.destroy
+  
+      redirect_to root_path, status: :see_other
+    end
+  
+    
+    private
+    def todo_params
+      params.require(:todo).permit(:title, :body)
+    end
+  end
+  
